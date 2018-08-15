65d6330e920b36b49aa715d993e86b69a01f9770
John Gould <johngould9487@gmail.com>
2018-08-15 11:35:01 +0100

    models, migrations and validations done

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant, presence: true
+
+  validates :content, :rating, presence: true
+  validates :rating, numericality: true
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
