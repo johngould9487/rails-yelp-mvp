53256af781c832427d2ffbc8fc8b8c073c0cad4e
John Gould <johngould9487@gmail.com>
2018-08-15 11:46:46 +0100

    seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants'
+restaurant_attributes = [
+  {
+    name: 'Mildreds',
+    address: 'London',
+    phone_number: '02089832422',
+    category: 'italian'
+  },
+  {
+    name: 'Etnosvet',
+    address: 'Prague',
+    phone_number: '68234897235',
+    category: 'chinese'
+  },
+  {
+    name: 'Bettys',
+    address: 'Amsterdam',
+    phone_number: '97834798333',
+    category: 'japanese'
+  },
+  {
+    name: 'Emma Pea',
+    address: 'Berlin',
+    phone_number: '7743873487333',
+    category: 'belgian'
+  },
+  {
+    name: '42 Degrees',
+    address: 'Paris',
+    phone_number: '9187348734',
+    category: 'french'
+  }
+]
+
+Restaurant.create!(restaurant_attributes)
+puts 'Finished!'
